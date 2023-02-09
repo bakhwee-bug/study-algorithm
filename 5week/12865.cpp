7d09e6eb02826544eb4cf9c9f71af07293ab3435
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2023-02-09 13:45:09 +0900

    5week 12865 평범한배낭

--- 5week/12865.cpp
+++ 5week/12865.cpp
@@ -1,26 +1,25 @@
-// 시간복잡도
-// 공간복잡도
-// 어려웠던 점:
-/**********************************
-
-**********************************/
-
 #include <iostream>
 #include <algorithm>
-#define MAX 1001
+#define MAX1 101
+#define MAX2 100001
 using namespace std;
 
 int N, K, W, V;
-
+int bag[MAX1][MAX2] = { 0 };
 void input() {
 	cin >> N >> K;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		cin >> W >> V;
+		for (int j = 1; j <= K; j++) {
+			if (W > j) bag[i][j] = bag[i - 1][j];
+			else bag[i][j] = max(bag[i - 1][j], (bag[i - 1][j-W] + V));
+		}
 	}
+	cout << bag[N][K];
 }
 
 
-
 int main() {
-
+	input();
+	return 0;
 }
