5f2d41a21709c511178a7feef7ebe23c9346897c
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2023-02-09 14:32:53 +0900

    6week 9084 동전 런타임에러 (OutOfBounds)

--- 6week/9084.cpp
+++ 6week/9084.cpp
@@ -5,17 +5,18 @@
 using namespace std;
 
 int T,N,M;
-int coin[MAX];
-int arr[MAX][MAX2];
 
 int main(){
     cin >> T;//테스트 케이스 입력 받기
     for(int k=0;k<T;k++){
+        int coin[MAX]={0};
+        int arr[MAX][MAX2]={0};
         cin >> N; //동전의 가지 수(1~20)
         for(int i=1;i<=N;i++){
             cin >> coin[i];//N가지 동전의 각 금액(1~10000)
         }
         cin >> M;//주어진 N가지 동전으로 만들어야 할 금액 M(1 ≤ M ≤ 10000)
+        
         for(int i=1;i<=N;i++){
             arr[coin[i]][0]=1;
             for(int j=1;j<=M;j++){
@@ -23,7 +24,7 @@ int main(){
                     arr[coin[i]][j]=arr[coin[i-1]][j];
                 }
                 else{
-                    arr[coin[i]][j] = arr[coin[i-1]][j] + arr[j-coin[i]][j];
+                    arr[coin[i]][j] = arr[coin[i-1]][j] + arr[coin[i]][j-coin[i]];
                 }
             }
         }
